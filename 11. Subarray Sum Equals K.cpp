f1cc2d7e5d37d6e9cac98ab12e8d138f714c42ce
Gracious Saxena <43841726+GraciousSaxena@users.noreply.github.com>
2020-08-22 00:01:10 +0530

    Create 11. Subarray Sum Equals K.cpp

---
+++ 11. Subarray Sum Equals K.cpp
@@ -0,0 +1,38 @@
+// Leetcode 560. Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+
+// Dimaag- Cumulative sum for each index, and difference between them to find the count!
+
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int[] sum = new int[nums.length + 1];
+        sum[0] = 0;
+        for (int i = 1; i <= nums.length; i++)
+            sum[i] = sum[i - 1] + nums[i - 1];
+        for (int start = 0; start < nums.length; start++) {
+            for (int end = start + 1; end <= nums.length; end++) {
+                if (sum[end] - sum[start] == k)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
+
+// Without Space
+// Dimaag evolve karo
+
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        for (int start = 0; start < nums.length; start++) {
+            int sum=0;
+            for (int end = start; end < nums.length; end++) {
+                sum+=nums[end];
+                if (sum == k)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
