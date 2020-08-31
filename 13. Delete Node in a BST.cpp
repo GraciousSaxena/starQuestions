8748044a728c307f8c63cf6094dd8227f3b90f1c
Gracious Saxena <43841726+GraciousSaxena@users.noreply.github.com>
2020-08-31 22:56:40 +0530

    Update 13. Delete Node in a BST.cpp

--- 13. Delete Node in a BST.cpp
+++ 13. Delete Node in a BST.cpp
@@ -18,16 +18,28 @@ public:
         } else if(root->val > key){
             root->left = deleteNode(root->left, key);
         } else {
+            
+//                 CRUX: inorder traversal of a BST gives a sorted array
+//                 We are trying to maintain that property
+            
+//             In case of a leaf node...
+//             proper deletion takes place here only in the entire code;
             if(root->left == NULL && root->right == NULL)
                 root = NULL;
             else if(root->right == NULL){
                 TreeNode* left = root->left;
+//                 find the inorder predecessor, or the element
+//                 just smaller than the node being deleted.
                 while(left->right != NULL){
                     left = left->right;
                 }
+//                 replace it's value to the key of root;
+//                 find and delete that node in the subtree;
                 root->val = left->val;
                 root->left = deleteNode(root->left, root->val);
             } else {
+//                 Find the inorder successor
+//                 The element just greater than the key
                 TreeNode* right = root->right;
                 while(right->left){
                     right = right->left;
